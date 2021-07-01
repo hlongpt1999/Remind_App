121de1a91bfbdbc7c34312062f2ca67c33badc4c
Phan Hải Long <phanhailong12345678@gmail.com>
2021-07-01 17:07:31 +0700

    W2 - D4: DataBindling
    Complete MVVM  Databinding in Login Activity

--- app/src/main/java/com/example/remind_app/LoginActivity.kt
+++ app/src/main/java/com/example/remind_app/LoginActivity.kt
@@ -1,17 +1,38 @@
 package com.example.remind_app
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.remind_app.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+//    val PACKAGE_CONTEXT:Context = applicationContext
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+
         var login : ActivityLoginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
-//        var loginViewModel = LoginViewModel()
-        login.loginViewModel
+        var loginViewModel = LoginViewModel()
+        login.loginViewModel = loginViewModel
+
+//        btnLogin.setOnClickListener(object : View.OnClickListener{
+//            override fun onClick(p0: View?) {
+//                moveToMain()
+//            }
+//        })
+    }
+
+    fun showToast(toast : String){
+        Toast.makeText(this@LoginActivity, toast, Toast.LENGTH_SHORT).show()
+    }
+
+    fun moveToMain(){
+        startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+
     }
 }
--- app/src/main/java/com/example/remind_app/LoginViewModel.kt
+++ app/src/main/java/com/example/remind_app/LoginViewModel.kt
@@ -1,13 +1,16 @@
 package com.example.remind_app
 
+import android.view.View
 import androidx.databinding.BaseObservable
 import androidx.databinding.Bindable
 import androidx.databinding.ObservableField
+import com.example.remind_app.Objects.User
 
 //VIEWMODEL in MVVM design pattern
-
 public class LoginViewModel : BaseObservable(){
-    var messageLogin = ObservableField<String>()
+        
+    var messageLogin: ObservableField<String> = ObservableField<String>()
+    var isShowMessage: ObservableField<Boolean> = ObservableField<Boolean>(false)
 
     @get: Bindable
     var username : String = ""
@@ -23,13 +26,26 @@ public class LoginViewModel : BaseObservable(){
         notifyPropertyChanged(BR.password)
     }
 
-    fun onClickLogin(){
-//        var user = User(username, password)
-//        if (user.username.equals("Admin") && user.password.equals("Admin")){
-//            messageLogin.set("Login success")
-//        }
-//        else
-            messageLogin.set("")
+
+
+    fun onClickLogin(view :View ){
+        var user = User(username, password)
+
+        if (username.equals("admin") && password.equals("admin")) {
+//            var context: Context = view.context
+//            var intent = Intent(context, MainActivity::class.java)
+//            context.startActivity(intent)
+
+            isShowMessage.set(false)
+        }
+        else {
+            messageLogin.set ("wrong")
+            isShowMessage.set(true)
+        }
+    }
+
+    fun hideMessage(){
+        isShowMessage.set(false)
     }
 
 }
